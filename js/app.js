3c0447f52ec31f568a9e096900b7e826a392a311
Ivan Paramonov <par-ivan@mail.ru>
2024-01-24 12:09:27 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+﻿gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 3,
+    effects: true
+  })
+
+  gsap.fromTo('.logo-section', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.logo-section',
+      start: 'center',
+      end: '820',
+      scrub: true
+    }
+  })
+
+  let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+  itemsL.forEach((item) => {
+    gsap.fromTo(item, { opacity: 0, x: -250 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+	start: '-1050',
+	end: '-100',
+        scrub: true
+      }
+    })
+  })
+
+  let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+  itemsR.forEach((item) => {
+    gsap.fromTo(item, { opacity: 0, x: 250 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+	start: '-1050',
+	end: '-100',
+        scrub: true
+      }
+    })
+  })
+}
